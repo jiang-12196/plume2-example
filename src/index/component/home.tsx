23ead2b96df4114d159874f7d039878819cf30ef
jiang-12196 <jiang12196@126.com>
2018-04-08 17:32:37 +0800

    add bujiadi test

--- src/index/component/home.tsx
+++ src/index/component/home.tsx
@@ -1,21 +1,52 @@
-import React from 'react';
-import { Relax } from 'plume2';
-import { withRouter } from 'react-router';
+import React from "react";
+import { Relax } from "plume2";
+import { withRouter } from "react-router";
+import { Radio, message } from "antd";
+import question from "./title";
+
+const RadioGroup = Radio.Group;
+
+console.log("question---->", question);
 
 @withRouter
 @Relax
 class Home extends React.Component<any, any> {
 	static relaxProps = {
-		text: 'text',
+		text: "text"
+	};
+
+	onChange = (e, answer) => {
+		console.log("radio checked", e.target.value);
+		if (e.target.value == answer) {
+			message.success("选择正确");
+			return;
+		}
+		message.error("选择错误");
+	};
+
+	getRadioGroup = () => {
+		return question.singleChoiceQuestion.map(item => {
+			return (
+				<div>
+					<span>题目：{item.title}</span>
+					<RadioGroup onChange={e => this.onChange(e, item.answer)}>
+						{this.getRaido(item.select)}
+					</RadioGroup>
+				</div>
+			);
+		});
+	};
+
+	getRaido = select => {
+		return Object.keys(select).map(key => (
+			<Radio key={key} value={key}>
+				{select[key]}
+			</Radio>
+		));
 	};
 
 	render() {
-		const { text } = this.props.relaxProps;
-		return (
-			<div className={'text'}>
-				<span>{text}</span>
-			</div>
-		);
+		return <div className="text">{this.getRadioGroup()}</div>;
 	}
 }
 
